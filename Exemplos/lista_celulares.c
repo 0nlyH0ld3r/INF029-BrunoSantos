99209395fcfd5aecb6c4090837b916445541be30
0nlyH0ld3r <manin.d2020@gmail.com>
2026-08-31 01:23:14 -0300

    Melhorias no código de /Exemplos/lista_celulares.c

--- Exemplos/lista_celulares.c
+++ Exemplos/lista_celulares.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+#include <termios.h>
+#include <unistd.h>
 
 typedef struct {
 	char nome[30];
@@ -7,10 +10,12 @@ typedef struct {
 	int preco;
 } Celular;
 
+int	input_char(); 
 void	clean_buffer();
 void	initialize_vector (Celular *lista_celulares, size_t size);
 void    input_string(char *string, size_t size);
 int	search_empty(Celular *lista_celulares, size_t size);
+
 int	inserir(Celular *lista_celulares, size_t tam);
 void	listar(Celular lista_celulares[], size_t size);
 void	atualizar(Celular *celular);
@@ -21,9 +26,10 @@ int main() {
 	Celular lista_celulares[tamanho];
 	int sair = 0;
 
+	system("clear");
 	initialize_vector(lista_celulares, tamanho);
 
-	while(!sair) {
+	do {
 		puts("Digite uma opção: \n");
 		puts("0: Sair\n");
 		puts("1: Inserir\n");
@@ -31,42 +37,76 @@ int main() {
 		puts("3: Atualizar\n");
 		puts("4: Deletar\n");
 
-		int opcao;
+		// int opcao;
 		int x;
-		scanf("%d", &opcao);
-		clean_buffer();
+		// scanf("%d", &opcao);
 
-		switch(opcao) {
+		switch(input_char() - '0') {
 			case 0: 
-				sair = 1;
+				return 0;
 				break;
+
 			case 1: 
+				system("clear");
 				switch(inserir(lista_celulares, tamanho)) {
 					case 0:
-						puts("Cadastro bem sucedido!\n");
+						puts("Cadastro bem sucedido!\n\n");
 						break;
 					case 1:
-						puts("Não há espaço para realizar o cadastro!\n");
+						puts("Não há espaço para realizar o cadastro!\n\n");
 						break;
 				}
 				break;
+
 			case 2: 
+				system("clear");
 				listar(lista_celulares, tamanho);
 				break;
+
 			case 3: 
+				system("clear");
 				puts("Qual celular deseja atualizar: ");
 				scanf("%d", &x);
 				clean_buffer();
 				atualizar(&lista_celulares[x]);
 				break;
+
 			case 4: 
-				puts("Qual celular deseja deletar: ");
+				system("clear");
+				puts("Digite o número do celular deseja deletar: "); // Criar atributo ID_Produto
 				scanf("%d", &x);
 				clean_buffer();
 				deletar(&lista_celulares[x]);
 				break;
+			default:
+				puts("Digite uma opção válida!\n");
+				break;
 		}
-	}
+
+		puts("Deseja sair do programa? (0 - Sair): ");
+
+		if (input_char() == '0')
+			sair = 1;
+
+		system("clear");
+
+	} while(!sair);
+}
+
+int input_char() {
+	struct termios old_t;
+	struct termios new_t;
+	tcgetattr(STDIN_FILENO, &old_t);
+	new_t = old_t;
+	new_t.c_lflag &= ~(ICANON | ECHO);
+
+	tcsetattr(STDIN_FILENO, TCSANOW, &new_t); // Muda o estado do terminal para retira o echo do input e pular o buffer
+						  // de escrita, redirecionando o input direto para a stream
+	int ch = getchar();
+
+	tcsetattr(STDIN_FILENO, TCSANOW, &old_t); // Reverte as mudanças
+
+	return ch;
 }
 
 void clean_buffer() {
@@ -99,19 +139,22 @@ int inserir(Celular *lista_celulares, size_t size) {
 	int slot;
 
 	if ((slot = search_empty(lista_celulares, size)) != -1) {
-		puts("\n\nDigite o nome do novo celular\n");
+		puts("Digite o nome do novo celular\n");
 		input_string(lista_celulares[slot].nome, 30);
 
+		puts("\n\n");
 		puts("Digite o ano do novo celular\n");
 		scanf("%d", &lista_celulares[slot].ano);
 		clean_buffer();
 
+		puts("\n\n");
 		puts("Digite o preço do novo celular\n");
 		scanf("%d", &lista_celulares[slot].preco);
 		clean_buffer();
 	}
 	else { return 1; } // Retornar 1 caso a lista esteja cheia
 
+	puts("\n\n");
 	puts("Celular cadastrado com sucesso!\n");
 	return 0; // Retornar 0 caso não haja nenhum erro
 }
@@ -121,24 +164,42 @@ void listar(Celular lista_celulares[], size_t size) {
 		if (lista_celulares[i].ano == -1) 
 			continue;
 		else
-			printf("%d - Nome: %s\nAno: %d\nPreço: %d\n\n", 
-					i, lista_celulares[i].nome, lista_celulares[i].ano, lista_celulares[i].preco);
+			printf("%d - NOME: %s\n\nAno: %d\nPreço: %d\n\n", i, lista_celulares[i].nome, lista_celulares[i].ano, lista_celulares[i].preco);
 	}
 	puts("\n");
 }
 
 void atualizar(Celular *celular) {
-	puts("\n\nDigite o novo nome do celular\n");
-	input_string(celular->nome, 30);
-	clean_buffer();
+	int op;
 
-	puts("Digite o novo ano do celular\n");
-	scanf("%d", &celular->ano);
 	clean_buffer();
+	puts("Deseja atualizar o nome? (s - sim) (n - não): ");
 
-	puts("Digite o novo preço do celular\n");
-	scanf("%d", &celular->preco);
-	clean_buffer();
+	if ((op = input_char()) == 's' || op == 'S') {
+		puts("\n\n");
+		puts("Digite o novo nome do celular\n");
+		input_string(celular->nome, 30);
+	}
+
+
+	puts("\nDeseja atualizar o ano? (s - sim) (n - não): ");
+
+	if ((op = input_char()) == 's' || op == 'S') {
+		puts("\n\n");
+		puts("Digite o novo ano do celular\n");
+		scanf("%d", &celular->ano);
+		clean_buffer();
+	}
+
+
+	puts("\nDeseja atualizar o preço? (s - sim) (n - não): ");
+
+	if ((op = input_char()) == 's' || op == 'S') {
+		puts("\n\n");
+		puts("Digite o novo preço do celular\n");
+		scanf("%d", &celular->preco);
+		clean_buffer();
+	}
 
 	printf("Atualização concluída!\n\n");
 }
